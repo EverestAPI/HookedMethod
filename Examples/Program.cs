8b7a43e1714b9151c9543054910a8d2e88c8062d
leo60228 <iakornfeld@gmail.com>
2018-02-18 10:08:46 -0500

    Improve example

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -4,6 +4,8 @@ using HookedMethod;
 
 public class main {
 	public static void Main() {
-		Console.WriteLine(((MethodInfo) (MethodInfoWithDef.FromCall(() => Console.WriteLine()))).Name);
+		MethodInfo method = MethodInfoWithDef.FromCall(() => Console.WriteLine());
+
+		Console.WriteLine(method.Name);
 	}
 }
