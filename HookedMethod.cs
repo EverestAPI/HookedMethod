92b2cbccd01b1213333c1c77dfb91919cdf5faad
Maik Macho <nuda1998@gmail.com>
2018-04-09 12:57:17 +0200

    Remove using MonoMod.Detour;

--- HookedMethod.cs
+++ HookedMethod.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
-using MonoMod.Detour;
 using MonoMod.RuntimeDetour;
 
 namespace HookedMethod
